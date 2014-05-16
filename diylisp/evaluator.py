97f24e59ceb15eb5733c5a0ef95d90ce9bd64b49
5u2ie <susie.bp@hotmail.com>
2014-05-16 14:24:52 +0200

    evaluating atoms + eq

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# nosetests tests/test_2_evaluating_simple_expressions.py --stop
 
 from types import Environment, LispError, Closure
 from ast import is_boolean, is_atom, is_symbol, is_list, is_closure, is_integer
@@ -16,4 +17,22 @@ in a day, after all.)
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
+
+    # evaluating booleans, integers, symbols and quotes
+    if is_boolean(ast):
+        return ast
+    elif is_integer(ast):
+        return ast
+    elif is_symbol(ast):
+        return ast
+    elif ast[0] == "quote":
+        return ast[1]
+
+    # evaluating atom and eq functions
+    if ast[0] == "atom":
+        return is_atom(evaluate(ast[1], env))
+    if ast[0] == "eq":
+        aste = [evaluate(s, env) for s in ast[1:]]
+        return is_atom(aste[0]) and aste[0] == aste[1]
+
     raise NotImplementedError("DIY")
